547ae4b6530996e29036d2128ba9d424dded3a21
Shubham <shubhamshibu42@gmail.com>
2025-02-23 09:15:47 +0530

    Completed

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -5,7 +5,7 @@ function Github() {
   const data = useLoaderData();
   // const [data, setData] = useState([])
   // useEffect(() => {
-  //  fetch('https://api.github.com/users/hiteshchoudhary')
+  //  fetch('https://api.github.com/users/')
   //  .then(response => response.json())
   //  .then(data => {
   //     console.log(data);
